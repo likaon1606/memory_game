7f0bd330365e2a26e483e902abdc125feac41706
Ariel <ariel.fuentesgarcia@gmail.com>
2023-06-20 16:52:33 -0600

    finish prototype

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import imgs from "./Images";
 import Card from "./Card";
+import Modal from "./Modal";
 
 const shuffleArray = (array) => {
   for (let i = array.length - 1; i > 0; i--) {
@@ -41,16 +42,94 @@ const Board = () => {
     createBoard();
   }, []);
 
+  const handleCardClick = (id) => {
+    //All Cards is Disabled
+    if (isDisabled) return;
+
+    // unstructure the array or give an error [currentCard]
+    const [currentCard] = cards.filter((card) => card.id === id);
+
+    if (!currentCard.flipped && !currentCard.matched) {
+      currentCard.flipped = true;
+
+      const newFlippedCards = [...flippedCards, currentCard];
+      setFlippedCards(newFlippedCards);
+
+      if (newFlippedCards.length === 2) {
+        setIsDisabled(true);
+        const [firstCard, secondCard] = newFlippedCards;
+
+        if (firstCard.img === secondCard.img) {
+          firstCard.matched = true;
+          secondCard.matched = true;
+          setIsDisabled(false);
+        } else {
+          setTimeout(() => {
+            firstCard.flipped = false;
+            secondCard.flipped = false;
+            setCards(cards);
+            setIsDisabled(false);
+          }, 1000);
+        }
+
+        setFlippedCards([]);
+        setMoves(moves + 1);
+      }
+
+      setCards(cards);
+    }
+
+    // Game Over
+    if (cards.every((card) => card.matched)) {
+      setGameOver(true);
+      setIsDisabled(true);
+    }
+  };
+
+  const handleNewGame = () => {
+    setCards([]);
+    createBoard();
+    setMoves(0);
+    setGameOver(false);
+    setIsDisabled(false);
+  };
+
   return (
-    <div className='relative h-screen flex items-center'>
-      <h1 className='font-bold text-4xl'>Memory Game</h1>
-      {/*Father Container Cards */}
-      <div className='grid grid-cols-4 gap-3 justify-center items-center px-3 py-5 my-3'>
-        {cards.map((card) => (
-          <Card card={card} key={card.id} />
-        ))}
+    <>
+      {gameOver && (
+        <div className='fixed inset-0 bg-black opacity-70 z-10'></div>
+      )}
+
+      <div className='relative h-screen flex items-center'>
+        <div className='mx-auto flex flex-col justify-center items-center'>
+          <h1 className='font-bold text-4xl my-8 text-center text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 transition duration-500 ease-transform hover:-translate-y-1 hover:scale-110 md:text-5xl '>
+            Memory Game
+          </h1>
+          {/*Father Container Cards */}
+          <div className='grid grid-cols-4 gap-3 justify-center items-center px-3 py-5 my-3'>
+            {cards.map((card) => (
+              <Card
+                card={card}
+                key={card.id}
+                handleCardClick={handleCardClick}
+              />
+            ))}
+          </div>
+          <button
+            className='bg-black font-semibold text-white rounded-md px-5 py-1 hover:bg-yellow-500 hover:text-black transition-all mb-3'
+            onClick={handleNewGame}
+          >
+            NewGame
+          </button>
+        </div>
+        <Modal
+          gameOver={gameOver}
+          setGameOver={setGameOver}
+          moves={moves}
+          handleNewGame={handleNewGame}
+        />
       </div>
-    </div>
+    </>
   );
 };
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-const Card = ({ card }) => {
+const Card = ({ card, handleCardClick }) => {
   return (
     <div
       className={`drop-shadow-md flex items-center ${
         card.flipped ? "[transform: rotateY(10deg)]" : "bg-white"
       } justify-center cursor-pointer h-16 w-16 hover:scale-105 rounded-xl transition-all duration-1000`}
+      onClick={() => handleCardClick(card.id)}
     >
       <div>
         <img
